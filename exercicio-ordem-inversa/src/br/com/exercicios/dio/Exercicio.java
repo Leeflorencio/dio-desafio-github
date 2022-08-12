9bdd2a694d7c4eb110534616d4e13fe0ebfc8d92
Leticia Florêncio <70420362+Leeflorencio@users.noreply.github.com>
2022-08-11 21:10:28 -0300

    Exercício de Arrays

---
+++ exercicio-ordem-inversa/src/br/com/exercicios/dio/Exercicio.java
@@ -0,0 +1,15 @@
+package br.com.exercicios.dio;
+
+public class Exercicio {
+
+    public static void main(String[] args){
+
+        double[] vetor2 = { 1.2, 2d, 4.4, 9.6, 8d};
+
+        int count = 0;
+        while(count < (vetor2.length)){
+            System.out.println(vetor2[count] + " ");
+            count ++;
+        }
+}
+}
